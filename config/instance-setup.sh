b2192f73ccdc45c004a96107cb92e4f3305e0898
Gabe Jackson <gabe@coffeepowered.co>
2019-07-31 15:18:56 -0400

    wip: fix replace issue

--- config/instance-setup.sh
+++ config/instance-setup.sh
@@ -8,9 +8,9 @@ wget https://example.com/mattermost-linux-amd64.tar.gz
 tar -zxvf mattermost-linux-amd64.tar.gz
 chown -R ubuntu mattermost
 cd mattermost
-sed -i'.bak1' 's|"DataSource": "[^"]*"|"DataSource": "postgres://mmuser:mostest@localhost:5432/mattermost?sslmode=disable\&connect_timeout=10"|g' config/s.Config.json
-sed -i'.bak2' 's|"DriverName": "mysql"|"DriverName": "postgres"|g' config/s.Config.json
-sed -i'.bak3' 's|"EnableDeveloper": false|"EnableDeveloper": true|g' config/s.Config.json
+sed -i'.bak1' 's|"DataSource": "[^"]*"|"DataSource": "postgres://mmuser:mostest@localhost:5432/mattermost?sslmode=disable\&connect_timeout=10"|g' config/Config.json
+sed -i'.bak2' 's|"DriverName": "mysql"|"DriverName": "postgres"|g' config/Config.json
+sed -i'.bak3' 's|"EnableDeveloper": false|"EnableDeveloper": true|g' config/Config.json
 ./bin/platform sampledata
 rm -f ./logs/mattermost.log # Required because of permissions issue
 start mattermost
